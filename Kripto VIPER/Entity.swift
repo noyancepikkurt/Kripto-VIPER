3556cb67bb3bc46d51d101e9d35b371306a500e1
Noyan Çepikkurt <cepikkurtnoyan@hotmail.com>
2022-10-28 19:15:26 +0300

    İlk defa VIPER ile bir uygulama geliştirdim. Hala kafamda soru işaretlerine karşılık VIPER mimarisi ile bir kaç proje daha yapacağım.

--- Kripto VIPER/Entity.swift
+++ Kripto VIPER/Entity.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+
+struct Crypto : Decodable {
+    
+    var currency : String
+    var price : String
+    
+}
--- Kripto VIPER/Presenter.swift
+++ Kripto VIPER/Presenter.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+
+enum NetworkError : Error {
+    case NetworkFailed
+    case ParsingFaied
+}
+
+
+protocol AnyPresenter {
+    var router : AnyRouter? {get set}
+    var interactor : AnyInteractor? {get set}
+    var view : AnyView? {get set}
+    
+    func interactorDidDownloadCrypto(result: Result<[Crypto], Error>)
+}
+
+class CryptoPresenter : AnyPresenter {
+    var router: AnyRouter?
+    
+    var interactor: AnyInteractor? {
+        didSet {
+            interactor?.downloadCryptos()
+        }
+    }
+    
+    var view: AnyView?
+    
+    func interactorDidDownloadCrypto(result: Result<[Crypto], Error>) {
+        switch result {
+        case.success(let cryptos) :
+            view?.update(with: cryptos)
+        
+        case.failure(_) :
+            view?.update(with: "Try again later...")
+        }
+    }
+    
+    
+}
