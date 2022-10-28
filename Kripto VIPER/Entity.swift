f853416f666502dceb14b9ada8f5d0e735cfa17a
Noyan Çepikkurt <cepikkurtnoyan@hotmail.com>
2022-10-28 17:29:22 +0300

    first commit

---
+++ Kripto VIPER/Entity.swift
@@ -0,0 +1,8 @@
+//
+//  Entity.swift
+//  Kripto VIPER
+//
+//  Created by Noyan Çepikkurt on 28.10.2022.
+//
+
+import Foundation
---
+++ Kripto VIPER/Interactor.swift
@@ -0,0 +1,8 @@
+//
+//  Interactor.swift
+//  Kripto VIPER
+//
+//  Created by Noyan Çepikkurt on 28.10.2022.
+//
+
+import Foundation
