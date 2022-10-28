2b20abdfd8f1c4701180ccc0835f03ee349b0940
Noyan Çepikkurt <cepikkurtnoyan@hotmail.com>
2022-10-28 17:21:49 +0300

    Initial Commit

---
+++ Kripto VIPER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kripto VIPER
+//
+//  Created by Noyan Çepikkurt on 28.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
